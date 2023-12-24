184019e1d8f1a369d2ae691591d5ad9735a5add9
Enyang Zhang <ezh15@uclive.ac.nz>
2023-12-24 19:44:47 +1300

    DT-2 [1h] Created about section. there is a white block to reserve the sidebar but haven't able to make it disappear when the sidebar collpse.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // not used
+// List of sentences
+var _CONTENT = [
+    "Hi, I'm Enyang Zhang.",
+    "I'm a Software Developer.",
+    "I like to create things.",
+];
+
+
 function on() {}
 
 //close sidebar
@@ -21,8 +29,11 @@ function toggleSidebar() {
     }
 }
 
-// function toggleSidebarBtn() {
-//     let sidebar = document.querySelector("#sidebar");
-//     sidebar.classList.toggle("active");
-//     document.getElementById("overlay").style.display = "none";
-// }
+function toggleSidebarBtn() {
+    let sidebar = document.querySelector("#sidebar");
+    sidebar.classList.toggle("active");
+    document.getElementById("overlay").style.display = "none";
+}
+
+
+
